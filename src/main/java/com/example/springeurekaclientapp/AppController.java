1d24f1b06a9b1e3326eeffc1c795cc84828e4f63
Xiao <831505@cognizant.com>
2019-12-31 13:23:59 -0500

    Initial commit

---
+++ src/main/java/com/example/springeurekaclientapp/AppController.java
@@ -0,0 +1,14 @@
+package com.example.springeurekaclientapp;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+    @RequestMapping(value="/process", method = RequestMethod.GET)
+    public @ResponseBody String  showLoginPage(ModelMap model){
+        return "Processing aap111111";
+    }
+}
---
+++ src/test/java/com/example/springeurekaclientapp/SpringEurekaClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springeurekaclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
